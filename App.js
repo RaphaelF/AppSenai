c23fa7b3a2eec24828c20b4c3e8c48370f23e268
Raphael <raphanallah@hotmail.com>
2021-11-11 13:58:14 -0300

    Seccond commit

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
+import {WebView} from 'react-native-webview'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source = {require('./assets/icon.png')}/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title= "Press" onPress = {() => console.log("button pressed")}/>
+      <Image source = {require('./assets/icon.png')}/>
     </View>
   );
 }
